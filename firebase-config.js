4162472435cecb406614d0ea7c480b9178f9c6c9
yashendy <yashendy@gmail.com>
2025-08-06 15:39:55 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGsk8luLKcP-9Mb9T76Lxxm3N3LooI72I",
+  authDomain: "school-results-9608d.firebaseapp.com",
+  databaseURL: "https://school-results-9608d-default-rtdb.firebaseio.com",
+  projectId: "school-results-9608d",
+  storageBucket: "school-results-9608d.appspot.com",
+  messagingSenderId: "381434456706",
+  appId: "1:381434456706:web:a68f63677598fceb7fe29b"
+};
+firebase.initializeApp(firebaseConfig);
