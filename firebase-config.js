0d85dd4f0de60cfe78fe8edc0d459eda3ab6715a
yashendy <yashendy@gmail.com>
2025-08-06 15:49:58 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+// firebase-config.js
+
+// 1. استيراد مكتبة Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCGsk8luLKcP-9Mb9T76Lxxm3N3LooI72I",
   authDomain: "school-results-9608d.firebaseapp.com",
@@ -7,4 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "381434456706",
   appId: "1:381434456706:web:a68f63677598fceb7fe29b"
 };
+
+// 2. تهيئة التطبيق
 firebase.initializeApp(firebaseConfig);
